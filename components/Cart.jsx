057a65da5dd466c115fac214c723ee4ed378eaf3
SouravvSen <souravv9142@gmail.com>
2025-10-26 13:09:45 +0530

    Revert "updated cart"
    This reverts commit 4f48334da73b591eec3a7b880ad2e76019ad9ee5.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -109,7 +109,7 @@ const Cart = () => {
 								<div className="item-desc">
 									<div className="flex top">
 										<h5>{item.name}</h5>
-										<h4>₹{item.price}</h4>
+										<h4>${item.price}</h4>
 									</div>
 									<div className="flex bottom">
 										<div>
@@ -149,7 +149,7 @@ const Cart = () => {
 					<div className="cart-bottom">
 						<div className="total">
 							<h3>Subtotal:</h3>
-							<h3>₹{totalPrice}</h3>
+							<h3>${totalPrice}</h3>
 						</div>
 						<div className="btn-container">
 							<button type="button" className="btn" onClick={handlePlaceOrder}>
